466357783426b2e7c025060a723894a37593a3ca
aahelexe <78219629+aahelexe@users.noreply.github.com>
2021-11-04 17:43:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//https://teachablemachine.withgoogle.com/models/cMa6dYTbW/model.json
+function startClassifier()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier= ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/cMa6dYTbW/model.json', modelReady);
+}
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+function gotResults(error, results)
